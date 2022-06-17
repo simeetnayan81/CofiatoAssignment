d85c1e5069a9b6bdb3b4c063ffd0827e2e1d82ae
Simeet Nayan <--get>
2022-06-17 22:35:09 +0530

    Small Correction

--- ImplementstrStr().cpp
+++ ImplementstrStr().cpp
@@ -1,10 +1,10 @@
- 
 class Solution {
 public:
     int strStr(string haystack, string needle) {
         int l=needle.length();
         if(l==0) return 0;
-        for(int i=0; i<=haystack.length()-l; i++){
+        for(int i=0; i<=(int)haystack.length()-l; i++){
+            cout<<haystack.length()-l;
             if(needle==haystack.substr(i, l))
                 return i;
         }
